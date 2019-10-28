bb51c3a350112e489d18dea0ebb2b94dd3a1697b
ali-sharif <ali@aion.network>
2019-10-28 14:55:10 -0400

    don't need to give classpath explicitly to in-memory compiler

--- zokrates-wrapper/src/main/java/org/aion/zokrates/AvmCompiler.java
+++ zokrates-wrapper/src/main/java/org/aion/zokrates/AvmCompiler.java
@@ -10,9 +10,6 @@ import java.util.*;
 
 public class AvmCompiler {
 
-    // hardcoded paths
-    private static final File avmLibPath = new File("../artifacts/avm");
-
     public static byte[] generateAvmJar(String mainClassFullyQualifiedName, Map<String, String> code) throws Exception {
         HashMap<String, byte[]> allClasses = compileInMemory(code);
         byte[] mainClass = allClasses.remove(mainClassFullyQualifiedName);
@@ -28,13 +25,9 @@ public class AvmCompiler {
     }
 
     private static HashMap<String, byte[]> compileInMemory(Map<String, String> sources) throws Exception {
-        String cp = buildClassPath(avmLibPath.getCanonicalPath() + "/*" );
-
         List<String> optionList = new ArrayList<>();
         optionList.add("--release");
         optionList.add("10");
-        optionList.add("-classpath");
-        optionList.add(System.getProperty("java.class.path") + ";" + cp);
 
         InMemoryJavaCompiler javac = InMemoryJavaCompiler.newInstance();
 
@@ -53,26 +46,4 @@ public class AvmCompiler {
 
         return classBytes;
     }
-
-    private static String buildClassPath(String... paths) {
-        StringBuilder sb = new StringBuilder();
-        for (String path : paths) {
-            if (path.endsWith("*")) {
-                path = path.substring(0, path.length() - 1);
-                File pathFile = new File(path);
-                for (File file : Objects.requireNonNull(pathFile.listFiles())) {
-                    if (file.isFile() && file.getName().endsWith(".jar")) {
-                        sb.append(path);
-                        sb.append(file.getName());
-                        sb.append(System.getProperty("path.separator"));
-                    }
-                }
-            } else {
-                sb.append(path);
-                sb.append(System.getProperty("path.separator"));
-            }
-        }
-        return sb.toString();
-    }
-
 }
